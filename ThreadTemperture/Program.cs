9154ca20d56d2583faab8c0cc7173285a0fa878c
sure0025 <sure0025@edu.eal.dk>
2017-01-31 11:39:54 +0100

    Add project files.

---
+++ ThreadTemperture/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTemperture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Start");
+
+            Thread thread = new Thread(new ThreadStart(Temperture.RandomGenerator));
+            thread.Start();
+
+
+            thread.Join();
+            thread.Abort();
+            Console.WriteLine("\nEnter for Exit");
+            Console.ReadLine();
+            Console.WriteLine("EXIT");
+        }
+    }
+}
---
+++ ThreadTemperture/Temperture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTemperture
+{
+    static class Temperture
+    {
+
+
+        public static void RandomGenerator()
+        {
+            Random random = new Random();
+            int alarmCount = 0;
+           
+
+            while (alarmCount < 3)
+            {
+                int randomTemperture = random.Next(-20, 121);
+                Console.WriteLine(randomTemperture);
+                Thread.Sleep(2000);
+
+                if (randomTemperture < 0 || randomTemperture > 100)
+                {
+                    Console.WriteLine("Alarm");
+                    alarmCount++; 
+                }
+            }
+
+        }
+
+    }
+}
